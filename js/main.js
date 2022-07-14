4f574927a041061fb02953e280d9d89f82bce7f3
Adrian Reyesta <muhammadadrianreyesta9d@gmail.com>
2022-07-14 21:00:05 +0700

    add responsive & debug

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$('.owl-one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.owl-two').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
